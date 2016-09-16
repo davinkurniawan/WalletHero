1f8d836638d07266834934388f49d1443c75b357
davinkurniawan <davinkurniawan07@gmail.com>
2016-09-16 23:05:20 +1000

    jdbc connection factory, login related dao and model

--- src/main/java/au/edu/unsw/comp4920/common/PostgreSQLDAOImpl.java
+++ src/main/java/au/edu/unsw/comp4920/common/PostgreSQLDAOImpl.java
@@ -1,18 +1,37 @@
 package au.edu.unsw.comp4920.common;
 
 import au.edu.unsw.comp4920.objects.*;
-import edu.unsw.comp9321.exception.DataAccessException;
-import edu.unsw.comp9321.exception.ServiceLocatorException;
+import au.edu.unsw.comp4920.exception.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Pattern;
+
+import javax.naming.NamingException;
 
 import au.edu.unsw.comp4920.common.DBConnectionFactory;
-import au.edu.unsw.comp4920.exception.*;
 
 public class PostgreSQLDAOImpl implements CommonDAO {
 
+	private DBConnectionFactory services;
+	Connection con;
+	
+	public PostgreSQLDAOImpl() {
+		services = new DBConnectionFactory();
+		try {
+			services.open();
+			con = services.getConnection();
+		} catch (ServiceLocatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	/*
 	 * public User getUserDetails(String username) { // TODO Auto-generated
 	 * method stub return null; }
@@ -20,9 +39,10 @@ public class PostgreSQLDAOImpl implements CommonDAO {
 	 * public User getUserDetails(int uid) { // TODO Auto-generated method stub
 	 * return null; }
 	 */
-
+	
 	@Override
 	public boolean createUser(User u) {
+		
 		// first test if user already exists
 		/*if (userExists(user.getUsername()) || emailExists(user.getEmail())) {
 			System.out.println("Not creating user because username and/or email already exists!");
@@ -74,9 +94,61 @@ public class PostgreSQLDAOImpl implements CommonDAO {
 	}
 
 	@Override
-	public User getUser(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+	public User getUser(String userinfo, String password) {
+		
+		User u = null;
+		String idQuery;
+		String passwordQuery;
+		
+		if (userinfo.contains("@")) {
+			idQuery = "email = " + userinfo;
+		} else { 
+			idQuery = "username = " + userinfo;
+		}
+		
+		if (password != null) {
+			passwordQuery = " and password = " + password + ";";
+		} else {
+			passwordQuery = ";";
+		}
+		
+		String query = "select * from user where " + idQuery
+				+ passwordQuery;
+		Statement statement;
+		try {
+			statement = con.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = statement.executeQuery(query);
+			if (rs.next()) {
+				//pull all information out of results and put it into userDTO
+				u = new User();
+				u.setPersonID(rs.getInt("id"));
+				u.setUsername(rs.getString("username"));
+				u.setPassword(rs.getString("email"));
+				u.setSalt(rs.getString("salt_hash"));
+				u.setFirstName(rs.getString("first_name"));
+				u.setMiddleName(rs.getString("middle_name"));
+				u.setLastName(rs.getString("last_name"));
+				u.setToken(rs.getString("token"));
+				u.setStatus_id(rs.getString("status_id"));
+				u.setBudget(rs.getDouble("budget"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+				services.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return u;
 	}
 
 }
