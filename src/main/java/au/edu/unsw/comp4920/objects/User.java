3331a210e5c307557649ea28fa8a3f066d91e1a1
davinkurniawan <davinkurniawan07@gmail.com>
2016-09-16 20:52:59 +1000

    getter and setter

--- src/main/java/au/edu/unsw/comp4920/objects/User.java
+++ src/main/java/au/edu/unsw/comp4920/objects/User.java
@@ -15,35 +15,89 @@ public class User {
 	private String token;
 	private String type;
 	
-	public User() {
-		
-	}
-	
-	public User(int personID, 
-				String username, 
-				String password, 
-				String nickName, 
-				String firstName, 
-				String lastName, 
-				String email, 
-				String address, 
-				String creditCard, 				
-				String token,
-				String type) {
-		super();
+	public User() {}
 
-		this.personID = personID;
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
 		this.nickName = nickName;
-		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public String getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(String creditCard) {
 		this.creditCard = creditCard;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
 		this.type = type;
 	}
+	
+	
 	
 	//TODO
 }
