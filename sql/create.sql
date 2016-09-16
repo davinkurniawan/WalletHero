5621cc3f60c975780083b4e70a2b5cabee855082
NataliaDj <nataliadjohari@yahoo.com.sg>
2016-09-16 18:11:31 +1000

    Fixed create and drop sql files

--- sql/create.sql
+++ sql/create.sql
@@ -1,53 +1,134 @@
-CREATE TABLE "User" (
-	"UserID" serial NOT NULL,
-	"Username" varchar(255) NOT NULL UNIQUE,
-	"Email" varchar(255) NOT NULL UNIQUE,
-	"Password" varchar(255) NOT NULL,
-	"SaltHash" varchar(255) NOT NULL,
-	"FirstName" varchar(255) NOT NULL,
-	"MiddleName" varchar(255),
-	"LastName" varchar(255) NOT NULL,
-	"Phone" varchar(255) NOT NULL,
-	"Token" varchar(255) NOT NULL,
-	"Age" bigint,
-	"Gender" char,
-	"Type" varchar(255) NOT NULL,
-	CONSTRAINT User_pk PRIMARY KEY ("UserID")
+
+CREATE TABLE "user" (
+	"id" serial NOT NULL,
+	"username" varchar NOT NULL UNIQUE,
+	"email" varchar NOT NULL UNIQUE,
+	"password" varchar NOT NULL,
+	"salt_hash" varchar NOT NULL,
+	"first_name" varchar NOT NULL,
+	"middle_name" varchar,
+	"last_name" varchar NOT NULL,
+	"token" varchar NOT NULL,
+	"status_id" int NOT NULL,
+	"budget" int NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Transactions" (
-	"TransactionID" serial NOT NULL,
-	"UserID" serial NOT NULL,
-	"Date" DATE NOT NULL,
-	"IsRecurring" BOOLEAN NOT NULL,
-	"RecurrenceFreq" bigint,
-	"ShortDescription" varchar NOT NULL,
-	"LongDescription" varchar,
-	"Price" DECIMAL NOT NULL,
-	"CategoryID" bigint,
-	CONSTRAINT Transactions_pk PRIMARY KEY ("TransactionID")
+CREATE TABLE "transaction" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"date" varchar NOT NULL,
+	"recur_id" int,
+	"detail" varchar,
+	"amount" DECIMAL NOT NULL,
+	"category_id" int,
+	"is_income" BOOLEAN NOT NULL,
+	CONSTRAINT transaction_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Session" (
-	"SessionID" serial(32) NOT NULL,
-	"UserID" bigint NOT NULL,
-	"LastAccessed" DATE NOT NULL,
-	CONSTRAINT Session_pk PRIMARY KEY ("SessionID")
+CREATE TABLE "session" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"last_access" varchar NOT NULL,
+	CONSTRAINT session_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
+CREATE TABLE "user_detail" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"currency" varchar,
+	"age" int,
+	"gender" char,
+	"occupation_id" int,
+	CONSTRAINT user_detail_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
 
-ALTER TABLE "Transactions" ADD CONSTRAINT "Transactions_fk0" FOREIGN KEY ("UserID") REFERENCES "User"("UserID");
 
-ALTER TABLE "Session" ADD CONSTRAINT "Session_fk0" FOREIGN KEY ("UserID") REFERENCES "User"("UserID");
+
+CREATE TABLE "category" (
+	"id" serial NOT NULL,
+	"name" varchar NOT NULL UNIQUE,
+	CONSTRAINT category_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "occupation" (
+	"id" serial NOT NULL,
+	"name" serial NOT NULL,
+	CONSTRAINT occupation_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "status" (
+	"id" serial NOT NULL,
+	"name" varchar NOT NULL,
+	CONSTRAINT status_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "recurrence" (
+	"id" serial NOT NULL,
+	"type" varchar NOT NULL UNIQUE,
+	"reccur_num" int,
+	"remaining" int,
+	CONSTRAINT recurrence_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "goal" (
+	"id" serial,
+	"user_id" int NOT NULL,
+	"recur_id" int NOT NULL,
+	"end_date" varchar NOT NULL,
+	"goal_amount" varchar NOT NULL,
+	"active" BOOLEAN NOT NULL,
+	CONSTRAINT goal_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "user" ADD CONSTRAINT "user_fk0" FOREIGN KEY ("status_id") REFERENCES "status"("id");
+
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk1" FOREIGN KEY ("recur_id") REFERENCES "recurrence"("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk2" FOREIGN KEY ("category_id") REFERENCES "category"("id");
+
+ALTER TABLE "session" ADD CONSTRAINT "session_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+
+ALTER TABLE "user_detail" ADD CONSTRAINT "user_detail_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "user_detail" ADD CONSTRAINT "user_detail_fk1" FOREIGN KEY ("occupation_id") REFERENCES "occupation"("id");
+
+
+
+
+
+ALTER TABLE "goal" ADD CONSTRAINT "goal_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "goal" ADD CONSTRAINT "goal_fk1" FOREIGN KEY ("recur_id") REFERENCES "recurrence"("id");
--- sql/drop.sql
+++ sql/drop.sql
@@ -1,9 +1,35 @@
-ALTER TABLE "Transactions" DROP CONSTRAINT IF EXISTS "Transactions_fk0";
+ALTER TABLE "user" DROP CONSTRAINT IF EXISTS "user_fk0";
 
-ALTER TABLE "Session" DROP CONSTRAINT IF EXISTS "Session_fk0";
+ALTER TABLE "transaction" DROP CONSTRAINT IF EXISTS "transaction_fk0";
 
-DROP TABLE IF EXISTS "User";
+ALTER TABLE "transaction" DROP CONSTRAINT IF EXISTS "transaction_fk1";
 
-DROP TABLE IF EXISTS "Transactions";
+ALTER TABLE "transaction" DROP CONSTRAINT IF EXISTS "transaction_fk2";
 
-DROP TABLE IF EXISTS "Session";
+ALTER TABLE "session" DROP CONSTRAINT IF EXISTS "session_fk0";
+
+ALTER TABLE "user_detail" DROP CONSTRAINT IF EXISTS "user_detail_fk0";
+
+ALTER TABLE "user_detail" DROP CONSTRAINT IF EXISTS "user_detail_fk1";
+
+ALTER TABLE "goal" DROP CONSTRAINT IF EXISTS "goal_fk0";
+
+ALTER TABLE "goal" DROP CONSTRAINT IF EXISTS "goal_fk1";
+
+DROP TABLE IF EXISTS "user";
+
+DROP TABLE IF EXISTS "transaction";
+
+DROP TABLE IF EXISTS "session";
+
+DROP TABLE IF EXISTS "user_detail";
+
+DROP TABLE IF EXISTS "category";
+
+DROP TABLE IF EXISTS "occupation";
+
+DROP TABLE IF EXISTS "status";
+
+DROP TABLE IF EXISTS "recurrence";
+
+DROP TABLE IF EXISTS "goal";
