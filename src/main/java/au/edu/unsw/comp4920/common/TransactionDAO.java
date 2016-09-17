360ebcdf5778f5114ff5df1810e182c3ba49a14c
sturner9001 <samuel.turner@live.com>
2016-09-17 05:42:39 +0000

    TransactionDAO.java deleted.

--- src/main/java/au/edu/unsw/comp4920/common/TransactionDAO.java
+++
@@ -1,8 +0,0 @@
-package au.edu.unsw.comp4920.common;
-
-import au.edu.unsw.comp4920.objects.*;
-
-public interface TransactionDAO {
-	public boolean addTransaction(Transaction t);
-	public Transaction getTransaction(int transactionID, int personID);
-}
