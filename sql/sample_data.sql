4f3629d991ecb2df4e296fd69ffc52649577336f
Timothy <Timothy@Timothys-MacBook-Air.local>
2016-09-20 12:47:09 +1000

    Fixed sql

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -4,8 +4,8 @@ INSERT INTO status (id, name) VALUES (3, 'Disabled');
 
 INSERT INTO occupation(id, name) VALUES(1, 'Student');
 
-INSERT INTO users (id, username, email, password, salt_hash, first_name, middle_name, last_name, token, status_id, budget) 
-	VALUES (1, 'wallethero', 'wallethero@wallethero.com', 'wallethero', 'gagas', 'Wallet', 'Something', 'Hero', 'lol', 2, 0.0);
+INSERT INTO users (username, email, password, salt_hash, first_name, middle_name, last_name, token, status_id, budget) 
+	VALUES ('wallethero', 'wallethero@wallethero.com', 'wallethero', 'gagas', 'Wallet', 'Something', 'Hero', 'lol', 2, 0.0);
 	
 INSERT INTO user_detail(id, user_id, currency, age, gender, occupation_id)
 	VALUES (1, 1, 'AUD', 22, 'M', 1);
