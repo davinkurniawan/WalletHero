f4d1389e5a00cc6264700379cccb55e4f1023977
Timothy <Timothy@Timothys-MacBook-Air.local>
2016-09-26 00:20:03 +1000

    Added extra files

---
+++ sql/real_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO status (id, name) VALUES (1, 'Inactive');
+INSERT INTO status (id, name) VALUES (2, 'Active');
+INSERT INTO status (id, name) VALUES (3, 'Disabled');
+
+INSERT INTO occupation(id, name) VALUES(1, 'Student');
