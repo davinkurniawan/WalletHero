a64f475cfda78434803746f9f8006878ee06f518
Timothy <Timothy@Timothys-MacBook-Air.local>
2016-09-26 23:55:27 +1000

    SQL update

--- sql/real_data.sql
+++ sql/real_data.sql
@@ -2,4 +2,22 @@ INSERT INTO status (id, name) VALUES (1, 'Inactive');
 INSERT INTO status (id, name) VALUES (2, 'Active');
 INSERT INTO status (id, name) VALUES (3, 'Disabled');
 
+INSERT INTO category VALUES (0,  'Others');
+INSERT INTO category VALUES (1,  'Business');
+INSERT INTO category VALUES (2,  'Interest');
+INSERT INTO category VALUES (3,  'Accounting/Legal');
+INSERT INTO category VALUES (4,  'Auto');
+INSERT INTO category VALUES (5,  'Capital Expenditure');
+INSERT INTO category VALUES (6,  'Education');
+INSERT INTO category VALUES (7,  'Food/Drink');
+INSERT INTO category VALUES (8,  'Health');
+INSERT INTO category VALUES (9,  'Maintenance');
+INSERT INTO category VALUES (10, 'Office');
+INSERT INTO category VALUES (11, 'Postage');
+INSERT INTO category VALUES (12, 'Properties');
+INSERT INTO category VALUES (13, 'Rent');
+INSERT INTO category VALUES (14, 'Taxes');
+INSERT INTO category VALUES (15, 'Telephone/Mobile');
+INSERT INTO category VALUES (16, 'Utilities');
+
 INSERT INTO occupation(id, name) VALUES(1, 'Student');
