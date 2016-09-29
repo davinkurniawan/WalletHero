9005665fa0dc77c4dc8977a202d7645ff762d5e4
Timothy <Timothy@Timothys-MacBook-Air.local>
2016-09-29 19:41:26 +1000

    Updated schema to support currency

--- sql/create.sql
+++ sql/create.sql
@@ -47,7 +47,7 @@ CREATE TABLE session (
 CREATE TABLE user_detail (
 	id serial NOT NULL,
 	user_id int NOT NULL,
-	currency varchar,
+	currency_id int NOT NULL,
 	age int,
 	gender char,
 	occupation_id int,
@@ -113,6 +113,19 @@ CREATE TABLE goal (
   OIDS=FALSE
 );
 
+
+
+CREATE TABLE currency (
+	id serial NOT NULL,
+	short_name varchar NOT NULL,
+	long_name varchar NOT NULL,
+	CONSTRAINT currency_pk PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 ALTER TABLE users ADD CONSTRAINT users_fk0 FOREIGN KEY (status_id) REFERENCES status(id);
 
 ALTER TABLE transaction ADD CONSTRAINT transaction_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
@@ -122,6 +135,7 @@ ALTER TABLE session ADD CONSTRAINT session_fk0 FOREIGN KEY (user_id) REFERENCES 
 
 ALTER TABLE user_detail ADD CONSTRAINT user_detail_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
 ALTER TABLE user_detail ADD CONSTRAINT user_detail_fk1 FOREIGN KEY (occupation_id) REFERENCES occupation(id);
+ALTER TABLE user_detail ADD CONSTRAINT user_detail_fk2 FOREIGN KEY (currency_id) REFERENCES currency(id);
 
 ALTER TABLE goal ADD CONSTRAINT goal_fk0 FOREIGN KEY (user_id) REFERENCES users(id);
 ALTER TABLE goal ADD CONSTRAINT goal_fk1 FOREIGN KEY (recur_id) REFERENCES recurrence(id);
@@ -149,3 +163,68 @@ INSERT INTO category VALUES (15, 'Telephone/Mobile');
 INSERT INTO category VALUES (16, 'Utilities');
 
 INSERT INTO occupation(id, name) VALUES(1, 'Student');
+
+INSERT INTO currency(id, short_name, long_name) VALUES(1, 'DZD', 'Algeria Dinars');
+INSERT INTO currency(id, short_name, long_name) VALUES(2, 'ARS', 'Argentina Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(3, 'AUD', 'Australia Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(4, 'BHD', 'Bahrain Dinars');
+INSERT INTO currency(id, short_name, long_name) VALUES(5, 'BRL', 'Brazil Reais');
+INSERT INTO currency(id, short_name, long_name) VALUES(6, 'BGN', 'Bulgaria Leva');
+INSERT INTO currency(id, short_name, long_name) VALUES(7, 'CAD', 'Canada Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(8, 'CLP', 'Chile Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(9, 'CNY', 'China Yuan Renminbi (RMB)');
+INSERT INTO currency(id, short_name, long_name) VALUES(10, 'COP', 'Colombia Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(11, 'CRC', 'Costa Rica Colones');
+INSERT INTO currency(id, short_name, long_name) VALUES(12, 'HRK', 'Croatia Kuna');
+INSERT INTO currency(id, short_name, long_name) VALUES(13, 'CZK', 'Czech Republic Koruny');
+INSERT INTO currency(id, short_name, long_name) VALUES(14, 'DKK', 'Denmark Kroner');
+INSERT INTO currency(id, short_name, long_name) VALUES(15, 'DOP', 'Dominican Republic Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(16, 'EGP', 'Egypt Pounds');
+INSERT INTO currency(id, short_name, long_name) VALUES(17, 'EEK', 'Estonia Krooni');
+INSERT INTO currency(id, short_name, long_name) VALUES(18, 'EUR', 'Euro');
+INSERT INTO currency(id, short_name, long_name) VALUES(19, 'FJD', 'Fiji Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(20, 'HKD', 'Hong Kong Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(21, 'HUF', 'Hungary Forint');
+INSERT INTO currency(id, short_name, long_name) VALUES(22, 'ISK', 'Iceland Kronur');
+INSERT INTO currency(id, short_name, long_name) VALUES(23, 'INR', 'India Rupees');
+INSERT INTO currency(id, short_name, long_name) VALUES(24, 'IDR', 'Indonesia Rupiahs');
+INSERT INTO currency(id, short_name, long_name) VALUES(25, 'ILS', 'Israel New Shekels');
+INSERT INTO currency(id, short_name, long_name) VALUES(26, 'JMD', 'Jamaica Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(27, 'JPY', 'Japan Yen');
+INSERT INTO currency(id, short_name, long_name) VALUES(28, 'JOD', 'Jordan Dinars');
+INSERT INTO currency(id, short_name, long_name) VALUES(29, 'KES', 'Kenya Shillings');
+INSERT INTO currency(id, short_name, long_name) VALUES(30, 'KWD', 'Kuwait Dinars');
+INSERT INTO currency(id, short_name, long_name) VALUES(31, 'LBP', 'Lebanon Pounds');
+INSERT INTO currency(id, short_name, long_name) VALUES(32, 'MYR', 'Malaysia Ringgits');
+INSERT INTO currency(id, short_name, long_name) VALUES(33, 'MUR', 'Mauritius Rupees');
+INSERT INTO currency(id, short_name, long_name) VALUES(34, 'MXN', 'Mexico Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(35, 'MAD', 'Morocco Dirhams');
+INSERT INTO currency(id, short_name, long_name) VALUES(36, 'NZD', 'New Zealand Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(37, 'NOK', 'Norway Kroner');
+INSERT INTO currency(id, short_name, long_name) VALUES(38, 'OMR', 'Oman Rials');
+INSERT INTO currency(id, short_name, long_name) VALUES(39, 'PKR', 'Pakistan Rupees');
+INSERT INTO currency(id, short_name, long_name) VALUES(40, 'PEN', 'Peru Nuevos Soles');
+INSERT INTO currency(id, short_name, long_name) VALUES(41, 'PHP', 'Philippines Pesos');
+INSERT INTO currency(id, short_name, long_name) VALUES(42, 'PLN', 'Poland Zlotych');
+INSERT INTO currency(id, short_name, long_name) VALUES(43, 'QAR', 'Qatar Riyals');
+INSERT INTO currency(id, short_name, long_name) VALUES(44, 'RON', 'Romania New Lei');
+INSERT INTO currency(id, short_name, long_name) VALUES(45, 'RUB', 'Russia Rubles');
+INSERT INTO currency(id, short_name, long_name) VALUES(46, 'SAR', 'Saudi Arabia Riyals');
+INSERT INTO currency(id, short_name, long_name) VALUES(47, 'SGD', 'Singapore Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(48, 'SKK', 'Slovakia Koruny');
+INSERT INTO currency(id, short_name, long_name) VALUES(49, 'ZAR', 'South Africa Rand');
+INSERT INTO currency(id, short_name, long_name) VALUES(50, 'KRW', 'South Korea Won');
+INSERT INTO currency(id, short_name, long_name) VALUES(51, 'LKR', 'Sri Lanka Rupees');
+INSERT INTO currency(id, short_name, long_name) VALUES(52, 'SEK', 'Sweden Kronor');
+INSERT INTO currency(id, short_name, long_name) VALUES(53, 'CHF', 'Switzerland Francs');
+INSERT INTO currency(id, short_name, long_name) VALUES(54, 'TWD', 'Taiwan New Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(55, 'THB', 'Thailand Baht');
+INSERT INTO currency(id, short_name, long_name) VALUES(56, 'TTD', 'Trinidad and Tobago Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(57, 'TND', 'Tunisia Dinars');
+INSERT INTO currency(id, short_name, long_name) VALUES(58, 'TRY', 'Turkey Lira');
+INSERT INTO currency(id, short_name, long_name) VALUES(59, 'AED', 'United Arab Emirates Dirhams');
+INSERT INTO currency(id, short_name, long_name) VALUES(60, 'GBP', 'United Kingdom Pounds');
+INSERT INTO currency(id, short_name, long_name) VALUES(61, 'USD', 'United States Dollars');
+INSERT INTO currency(id, short_name, long_name) VALUES(62, 'VEB', 'Venezuela Bolivares');
+INSERT INTO currency(id, short_name, long_name) VALUES(63, 'VND', 'Vietnam Dong');
+INSERT INTO currency(id, short_name, long_name) VALUES(64, 'ZMK', 'Zambia Kwacha');
